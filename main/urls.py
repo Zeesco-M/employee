7c04fb1a7547873aef58b1c90ca72707e1ba16e8
Zessco-M <tobiisrael715@gmail.com>
2025-08-15 01:43:26 +0100

    views.py, urls.py, settings.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect
 
 def redirect_to_employee(request):
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', redirect_to_employee),
     path('employee/', include('employment.urls')),
     path('', lambda request: redirect('land')),
-    
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]    
 
 
